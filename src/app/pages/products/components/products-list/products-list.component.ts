c6eb1cb2249145c6c52355924bcbb797caadf57b
Carlos <carlosempd@gmail.com>
2023-09-27 17:23:06 -0400

    ✨ faet: added loader

--- src/app/pages/products/components/products-list/products-list.component.ts
+++ src/app/pages/products/components/products-list/products-list.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable, map, switchMap, tap } from 'rxjs';
 import { Product } from 'src/app/core/interfaces/product.interface';
 import { PaginatedResponse, Pagination } from 'src/app/core/interfaces/response.interface';
 import { ProductService } from 'src/app/core/services/product.service';
@@ -29,9 +29,11 @@ export class ProductsListComponent implements OnInit, AfterViewInit {
     this.datasource = new MatTableDataSource<Product>(this.products);
     this.search$ = this.productService.searchText$;
     this.search$.pipe(
+      tap(() => { this.loadingData = true }),
       switchMap(value => this.productService.getProducts(undefined, value)),
       map((response: PaginatedResponse) => {
         this.resultsLength = response.totalCount;
+        this.loadingData = false;
         return response.data
       })
     ).subscribe((res: Product[]) => {
@@ -41,6 +43,7 @@ export class ProductsListComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.paginator.page.pipe(
+      tap(() => { this.loadingData = true }),
       map((pageEvent: PageEvent) => ({
         currentPage: pageEvent.pageIndex+1,
         totalPages: pageEvent.length,
@@ -49,6 +52,7 @@ export class ProductsListComponent implements OnInit, AfterViewInit {
       })),
       switchMap((pagination: Pagination) => this.productService.getProducts(pagination)),
       map((response: PaginatedResponse) => {
+        this.loadingData = false;
         this.resultsLength = response.totalCount;
         return response.data
       })
