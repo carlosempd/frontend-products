0f8f54878dbb270ae8c6265629e383a75856f53d
Carlos <carlosempd@gmail.com>
2023-09-27 10:57:07 -0400

    ✨ feat: added filter component

---
+++ src/app/pages/products/components/search/search.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, debounceTime, distinctUntilChanged, map } from 'rxjs';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
+  searchText$: Observable<string>;
+  @Output() searchEmitter: EventEmitter<Observable<string>> = new EventEmitter<Observable<string>>;
+ 
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      name: ['']
+    });
+
+    this.searchText$ = this.searchForm.valueChanges
+      .pipe(
+        debounceTime(400),
+        map(val => val.name),
+        distinctUntilChanged()
+      );
+
+    this.searchEmitter.emit(this.searchText$);
+  }
+}
